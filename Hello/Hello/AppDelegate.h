485f93d4d77ddc60b6f60582f8e38d36fd8108ae
brijeshgupta1329 <brijesh@unikove.com>
2018-06-05 13:06:08 +0530

    Add files via upload

---
+++ Hello/Hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello
+//
+//  Created by kiwitech on 05/06/18.
+//  Copyright © 2018 kiwitech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
